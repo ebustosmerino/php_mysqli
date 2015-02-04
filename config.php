0c2728c3bc0967afdc74c617e197aa43bcf98032
Eduardo Bustos <ebustosmerino@gmail.com>
2015-02-04 18:17:05 -0300

    first commit

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+  $server="localhost";
+  // Si el servidor tiene cpanel normalmente va primero tu nombre de username,
+  // por ejemplo: username_nombredelabasededatos
+  $database = "test";
+  $db_user = "admin";
+  $db_pass = "admin";
+?> 
