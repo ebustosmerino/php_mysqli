75c99c6c0ec4412e29d268f09e9cd294cc282eaa
ebustosmerino <ebustosmerino@gmail.com>
2015-03-11 11:31:56 -0300

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
-  $server="localhost";
-  // Si el servidor tiene cpanel normalmente va primero tu nombre de username,
-  // por ejemplo: username_nombredelabasededatos
-  $database = "test";
-  $db_user = "admin";
-  $db_pass = "admin";
+    $server="localhost";
+    // Si el servidor tiene cpanel normalmente va primero el nombre de usuario,
+    // por ejemplo: username_namedatabase
+    $database = "name_database";
+    $db_user = "name_user";
+    $db_pass = "user_pass";
 ?> 
